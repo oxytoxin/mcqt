3b54c4191c19825aa2c477918ede44cb0c507f53
oxytoxin <oxytoxinsgrace@gmail.com>
2022-11-11 02:22:25 +0800

    wip

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [MiscellaneousController::class, 'index'])->name('categories.index');
+Route::get('/', [MiscellaneousController::class, 'index']);
 
 Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)
