dd94d005b818bfd554282ca19e59fa8fbce4dbd3
oxytoxin <oxytoxinsgrace@gmail.com>
2023-03-24 10:45:42 +0800

    lol

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
